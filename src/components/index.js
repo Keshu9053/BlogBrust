0c511781b048f6731d8952ecb9d8a9ed94ceee19
Keshu9053 <103336493+Keshu9053@users.noreply.github.com>
2023-12-25 23:06:56 +0530

    hi

--- src/components/index.js
+++ src/components/index.js
@@ -5,8 +5,10 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
-import Signup from "./Signup";
+
 import Login from "./Login";
+import Signup from "./Signup";
+
 import Button from "./Button";
 import PostForm from "./postform/PostForm";
 import PostCard from "./PostCard";
