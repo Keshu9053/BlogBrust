c10228f225e202a34a55b29ca932648999f9a5e2
Keshav Bhardwaj <103336493+Keshu9053@users.noreply.github.com>
2024-06-16 12:05:06 +0530

    Update RTE.jsx

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,31 +1,28 @@
-import React from 'react'
-import {Editor } from '@tinymce/tinymce-react';
-import {Controller } from 'react-hook-form';
+import React from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+import { Controller } from 'react-hook-form';
 
-
-export default function RTE({name, control, label, defaultValue =""}) {
+export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
-    <div className='w-full h-full'> 
-    {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
-
-    <Controller
-    name={name || "content"}
-    control={control}
-    render={({field: {onChange}}) => (
-        <Editor
-        apiKey='gg7uyvpib2i73e7ca1uipjb7u5tbkqd3kpbh27rzd2i98e6g'
-        initialValue={defaultValue}
-        init={{
-            initialValue: defaultValue,
-            height: 500,
-            menubar: true,
-            plugins: [
+    <div className='w-full h-full'>
+      {label && <label className='inline-block mb-1 pl-1'>{label}</label>}
+      <Controller
+        name={name || "content"}
+        control={control}
+        defaultValue={defaultValue} // Ensure defaultValue is passed here
+        render={({ field: { onChange, value } }) => (
+          <Editor
+            apiKey='gg7uyvpib2i73e7ca1uipjb7u5tbkqd3kpbh27rzd2i98e6g'
+            value={value} // Bind value to the Editor
+            init={{
+              height: 500,
+              menubar: true,
+              plugins: [
                 "image",
                 "advlist",
                 "autolink",
                 "lists",
                 "link",
-                "image",
                 "charmap",
                 "preview",
                 "anchor",
@@ -36,20 +33,17 @@ export default function RTE({name, control, label, defaultValue =""}) {
                 "insertdatetime",
                 "media",
                 "table",
-                "code",
                 "help",
                 "wordcount",
-                "anchor",
-            ],
-            toolbar:
-            "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent |removeformat | help",
-            content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
-        }}
-        onEditorChange={onChange}
-        />
-    )}
-    />
-
-     </div>
-  )
+              ],
+              toolbar:
+                "undo redo | blocks | image | bold italic forecolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help",
+              content_style: "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }"
+            }}
+            onEditorChange={onChange}
+          />
+        )}
+      />
+    </div>
+  );
 }
